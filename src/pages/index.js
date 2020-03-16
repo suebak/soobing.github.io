7daa4dc822ba93e8acfe16c4827cfedfea1419b3
soobing <qls0147@naver.com>
2020-03-17 01:34:13 +0900

    react-p5 install

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from "react"
+import Sketch from "react-p5";
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  let x = 50;
+  let y = 50;
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(500, 500).parent(canvasParentRef); // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
+  };
+  const draw = p5 => {
+    p5.background(0);
+    p5.ellipse(x, y, 70, 70);
+    // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
+    x++;
+  };
+  return <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -14,8 +26,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Sketch setup={setup} draw={draw} />;
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+}
 
 export default IndexPage
