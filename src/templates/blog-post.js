29fc891b0b5740ed891b8fe685eacb613d417f36
soobing <qls0147@naver.com>
2020-03-18 14:55:08 +0900

    title 변경

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,7 +8,7 @@ export default function Template({ data }) {
   const { markdownRemark: post } = data
   return (
     <Layout className="blog-post-container">
-      <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
+      <Helmet title={`${post.frontmatter.title} :: Soobing's Story`} />
       <div className="blog-post">
         <h1>{post.frontmatter.title}</h1>
         <div
